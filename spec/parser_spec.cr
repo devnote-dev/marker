ca882caa30f4dfd3e1365ae05ff9a2c9c300eca2
devnote-dev <devnote.dev75@gmail.com>
2023-06-07 01:57:29 +0100

    spec(parser): add strong and emphasis specs

--- spec/parser_spec.cr
+++ spec/parser_spec.cr
@@ -92,4 +92,98 @@ describe Parser do
       para.value[0].as(CMark::Text).value.should eq "on two lines"
     end
   end
+
+  describe CMark::Strong do
+    it "parses strong text" do
+      nodes = parse("this is **strong**")
+
+      nodes.size.should eq 1
+      nodes[0].should be_a CMark::Paragraph
+      para = nodes[0].as(CMark::Paragraph)
+
+      para.value.size.should eq 2
+      para.value[0].should be_a CMark::Text
+      para.value[0].as(CMark::Text).value.should eq "this is "
+      para.value[1].should be_a CMark::Strong
+      strong = para.value[1].as(CMark::Strong)
+
+      strong.value.size.should eq 1
+      strong.value[0].should be_a CMark::Text
+      strong.value[0].as(CMark::Text).value.should eq "strong"
+    end
+
+    it "parses nested text types" do
+      nodes = parse("this is **_kinda_ strong**")
+
+      nodes.size.should eq 1
+      nodes[0].should be_a CMark::Paragraph
+      para = nodes[0].as(CMark::Paragraph)
+
+      para.value.size.should eq 2
+      para.value[0].should be_a CMark::Text
+      para.value[0].as(CMark::Text).value.should eq "this is "
+      para.value[1].should be_a CMark::Strong
+      strong = para.value[1].as(CMark::Strong)
+
+      strong.value.size.should eq 3
+      strong.value[0].should be_a CMark::Emphasis
+      emph = strong.value[0].as(CMark::Emphasis)
+
+      emph.value.size.should eq 1
+      emph.value[0].should be_a CMark::Text
+      emph.value[0].as(CMark::Text).value.should eq "kinda"
+
+      strong.value[1].should be_a CMark::Text
+      strong.value[1].as(CMark::Text).value.should eq " "
+      strong.value[2].should be_a CMark::Text
+      strong.value[2].as(CMark::Text).value.should eq "strong"
+    end
+  end
+
+  describe CMark::Emphasis do
+    it "parses emphatic text" do
+      nodes = parse("this is _emphatic_")
+
+      nodes.size.should eq 1
+      nodes[0].should be_a CMark::Paragraph
+      para = nodes[0].as(CMark::Paragraph)
+
+      para.value.size.should eq 2
+      para.value[0].should be_a CMark::Text
+      para.value[0].as(CMark::Text).value.should eq "this is "
+      para.value[1].should be_a CMark::Emphasis
+      emph = para.value[1].as(CMark::Emphasis)
+
+      emph.value.size.should eq 1
+      emph.value[0].should be_a CMark::Text
+      emph.value[0].as(CMark::Text).value.should eq "emphatic"
+    end
+
+    it "parses nested text types" do
+      nodes = parse("this is _**very** emphatic_")
+
+      nodes.size.should eq 1
+      nodes[0].should be_a CMark::Paragraph
+      para = nodes[0].as(CMark::Paragraph)
+
+      para.value.size.should eq 2
+      para.value[0].should be_a CMark::Text
+      para.value[0].as(CMark::Text).value.should eq "this is "
+      para.value[1].should be_a CMark::Emphasis
+      emph = para.value[1].as(CMark::Emphasis)
+
+      emph.value.size.should eq 3
+      emph.value[0].should be_a CMark::Strong
+      strong = emph.value[0].as(CMark::Strong)
+
+      strong.value.size.should eq 1
+      strong.value[0].should be_a CMark::Text
+      strong.value[0].as(CMark::Text).value.should eq "very"
+
+      emph.value[1].should be_a CMark::Text
+      emph.value[1].as(CMark::Text).value.should eq " "
+      emph.value[2].should be_a CMark::Text
+      emph.value[2].as(CMark::Text).value.should eq "emphatic"
+    end
+  end
 end
