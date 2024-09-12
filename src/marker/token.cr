67debeedb0ec7588faaff3b35e21f1432df67fa9
devnote-dev <devnote.dev75@gmail.com>
2024-09-12 16:23:45 +0100

    refactor: impl location, reimpl token & kinds

--- src/marker/token.cr
+++ src/marker/token.cr
@@ -1,57 +1,85 @@
 module Marker
-  # class Location
-  #   @loc : StaticArray(Int32, 4)
-  #
-  #   def self.[](line : Int32, column : Int32)
-  #     new StaticArray[line, line, column, column]
-  #   end
-  #
-  #   def initialize(@loc : StaticArray(Int32, 4))
-  #   end
-  #
-  #   def line : {Int32, Int32}
-  #     {@loc[0], @loc[1]}
-  #   end
-  #
-  #   def column : {Int32, Int32}
-  #     {@loc[2], @loc[3]}
-  #   end
-  #
-  #   def set_end(line : Int32, column : Int32) : Nil
-  #     @loc[1] = line
-  #     @loc[3] = column
-  #   end
-  # end
+  class Location
+    @value : StaticArray(Int32, 4)
 
-  class Token
+    def self.[](line : Int32, column : Int32)
+      new StaticArray[linel, column, line, column]
+    end
+
+    def initialize(@value : StaticArray(Int32, 4))
+    end
+
+    def start : {Int32, Int32}
+      {@value[0], @value[1]}
+    end
+
+    def end : {Int32, Int32}
+      {@value[2], @value[3]}
+    end
+
+    def start_at(line : Int32, column : Int32) : self
+      @value[0] = line
+      @value[1] = column
+
+      self
+    end
+
+    def end_at(line : Int32, column : Int32) : self
+      @value[2] = line
+      @value[3] = column
+
+      self
+    end
+
+    def &(other : Location) : Location
+      Location[@value[0], @value[1]].end_at(*other.end)
+    end
+  end
+
+  struct Token
     enum Kind
-      Heading    # #
-      Text       # Aa
-      Strong     # ** or __
-      Emphasis   # * or _
-      CodeSpan   # ``
-      CodeBlock  # ``` or ~~~
-      HTMLBlock  # <!-- -->
-      BlockQuote # >
-      ListItem   # - or * or 1-9.
-
-      # LeftBracket  # [
-      # RightBracket # ]
-      # LeftParen    # (
-      # RightParen   # )
-      # Colon        # :
-
+      EOF
       Space
       Newline
       Escape
-      EOF
+
+      # Leaf block tokens
+
+      ThematicBreak     # (?<!\s{4})(-|_|\*)(?:\1|\s){2,}$
+      ATXHeading        # (?<!\s{4})(#{1,6}(?<!#{7}))\s+.+$
+      SetextHeading     # (?<!\s{4})===+$
+      IndentedCodeBlock # \s{4,}.*$
+      FencedCodeBlock   # (?<!\s{4})(`|~)\1{2,}.*$
+      HTMLBlock         # </> <!-- -->
+
+      # Container block tokens
+
+      BlockQuote      # >
+      BulletListItem  # - or + or *
+      OrderedListItem # 0-9. or 0-9)
+
+      # Inline tokens
+
+      Backtick     # `
+      Asterisk     # *
+      Underscore   # _
+      LeftBracket  # [
+      RightBracket # ]
+      LeftParen    # (
+      RightParen   # )
+      LeftAngle    # <
+      RightAngle   # >
+      Quote        # "" or ''
+      Colon        # :
+      Bang         # !
+      Text
     end
 
-    property kind : Kind
-    # getter loc : Location
+    getter kind : Kind
+    getter loc : Location
     @value : String?
 
-    def initialize(@kind : Kind, @value : String? = nil)
+    def initialize(@kind : Kind, @loc : Location, @value : String? = nil)
     end
 
     def value : String
