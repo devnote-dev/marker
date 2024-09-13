d34b7ea4f268d9c967925350a2d53159b449e064
devnote-dev <devnote.dev75@gmail.com>
2024-09-13 08:02:21 +0100

    feat(spec): add most coverage for thematic break

--- spec/parser_spec.cr
+++ spec/parser_spec.cr
@@ -101,4 +101,100 @@ describe Marker::Parser do
       node.value.should eq "foo  "
     end
   end
+
+  context "parses thematic breaks:" do
+    it "example 43" do
+      nodes = parse <<-MD
+        ***
+        ---
+        ___
+        MD
+
+      nodes.size.should eq 3
+      node = nodes[0].should be_a Marker::ThematicBreak
+
+      node.kind.should eq Marker::ThematicBreak::Kind::Asterisk
+      node.size.should eq 3
+      node = nodes[1].should be_a Marker::ThematicBreak
+
+      node.kind.should eq Marker::ThematicBreak::Kind::Dash
+      node.size.should eq 3
+      node = nodes[2].should be_a Marker::ThematicBreak
+
+      node.kind.should eq Marker::ThematicBreak::Kind::Underscore
+      node.size.should eq 3
+    end
+
+    it "example 47" do
+      nodes = parse <<-MD
+         ***
+          ---
+           ___
+        MD
+
+      nodes.size.should eq 3
+      node = nodes[0].should be_a Marker::ThematicBreak
+
+      node.kind.should eq Marker::ThematicBreak::Kind::Asterisk
+      node.size.should eq 3
+      node = nodes[1].should be_a Marker::ThematicBreak
+
+      node.kind.should eq Marker::ThematicBreak::Kind::Dash
+      node.size.should eq 3
+      node = nodes[2].should be_a Marker::ThematicBreak
+
+      node.kind.should eq Marker::ThematicBreak::Kind::Underscore
+      node.size.should eq 3
+    end
+
+    it "example 50" do
+      nodes = parse "_____________________________________"
+
+      nodes.size.should eq 1
+      node = nodes[0].should be_a Marker::ThematicBreak
+
+      node.kind.should eq Marker::ThematicBreak::Kind::Underscore
+      node.size.should eq 37
+    end
+
+    pending "example 51" do
+      nodes = parse " - - -"
+
+      nodes.size.should eq 1
+      node = nodes[0].should be_a Marker::ThematicBreak
+
+      node.kind.should eq Marker::ThematicBreak::Kind::Dash
+      node.size.should eq 6
+    end
+
+    pending "example 52" do
+      nodes = parse " **  * ** * ** * **"
+
+      nodes.size.should eq 1
+      node = nodes[0].should be_a Marker::ThematicBreak
+
+      node.kind.should eq Marker::ThematicBreak::Kind::Asterisk
+      node.size.should eq 19
+    end
+
+    pending "example 53" do
+      nodes = parse "-     -      -      -"
+
+      nodes.size.should eq 1
+      node = nodes[0].should be_a Marker::ThematicBreak
+
+      node.kind.should eq Marker::ThematicBreak::Kind::Dash
+      node.size.should eq 21
+    end
+
+    pending "example 54" do
+      nodes = parse "- - - -    "
+
+      nodes.size.should eq 1
+      node = nodes[0].should be_a Marker::ThematicBreak
+
+      node.kind.should eq Marker::ThematicBreak::Kind::Dash
+      node.size.should eq 6
+    end
+  end
 end
