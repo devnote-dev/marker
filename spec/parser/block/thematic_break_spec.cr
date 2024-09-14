639511dbfc2d525bc1c7a01e5d7a5483909fa3ac
devnote-dev <devnote.dev75@gmail.com>
2024-09-14 19:41:45 +0100

    chore(spec): regrouping

--- spec/parser/block/thematic_break_spec.cr
+++ spec/parser/block/thematic_break_spec.cr
@@ -1,4 +1,4 @@
-require "../spec_helper"
+require "../../spec_helper"
 
 describe Marker::Parser do
   context "parses thematic breaks:" do
--- spec/parser/inline/code_span_spec.cr
+++ spec/parser/inline/code_span_spec.cr
@@ -1,7 +1,7 @@
-require "../spec_helper"
+require "../../spec_helper"
 
 describe Marker::Parser do
-  context "parses inlines - code spans:" do
+  context "parses code spans:" do
     it "example 327" do
       nodes = parse "`hi`lo"
 
