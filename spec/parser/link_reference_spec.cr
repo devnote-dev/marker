443cd7b6d58ea651654b513a63c857ccd72a5118
devnote-dev <devnote.dev75@gmail.com>
2024-09-14 03:01:50 +0100

    feat(spec): add partial coverage for link reference

---
+++ spec/parser/link_reference_spec.cr
@@ -0,0 +1,39 @@
+require "../spec_helper"
+
+describe Marker::Parser do
+  context "parses link reference definitions:" do
+    it "example 192 (partial)" do
+      nodes = parse <<-MD
+        [foo]: /url "title"
+        MD
+
+      nodes.size.should eq 1
+      node = nodes[0].should be_a Marker::LinkReference
+
+      node.label.size.should eq 1
+      text = node.label[0].should be_a Marker::Text
+
+      text.value.should eq "foo"
+      node.destination.should eq "/url"
+      node.title.should eq "title"
+    end
+
+    pending "example 193 (partial)" do
+      nodes = parse <<-MD
+          [foo]: 
+            /url  
+                'the title'  
+        MD
+
+      nodes.size.should eq 1
+      node = nodes[0].should be_a Marker::LinkReference
+
+      node.label.size.should eq 1
+      text = node.label[0].should be_a Marker::Text
+
+      text.value.should eq "foo"
+      node.destination.should eq "/url"
+      node.title.should eq "the title"
+    end
+  end
+end
